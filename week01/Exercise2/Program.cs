7593d5197c5bbeb7cc3131eac3aafd9cad961fe2
ILE <j_aikido@yahoo.com>
2025-01-16 16:08:38 +0100

    Modified code in response to weekly Exercise1 Modified code Lines

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        if (percentage >= 90)
+            Console.WriteLine("Your grade is A.");
+        else if (percentage >= 80)
+            Console.WriteLine("Your grade is B.");
+        else if (percentage >=70)
+            Console.WriteLine("Your grade is C.");
+        else if (percentage >= 60)
+            Console.WriteLine("Your grade is D.");
+        else if (percentage < 60)
+            Console.WriteLine("Your grade is F.");
+            
     }
 }
